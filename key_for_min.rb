4ad780040320f29c87b68a8c666f0f3d6aa00f51
Chris Nady <nady.christopher@gmail.com>
2017-09-07 19:16:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_value = Integer.new
-  min_value = name_hash.first_value
+  min_value = name_hash.values[0]
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
