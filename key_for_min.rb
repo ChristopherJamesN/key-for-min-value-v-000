f32e042082ef9239b74eadc72c00ac9ae1fc0ddd
Chris Nady <nady.christopher@gmail.com>
2017-09-07 19:24:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,15 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
+  min_key = nil
   name_hash.each do |key, value|
     if min_value == nil
       min_value = value
+      min_key=key
     elsif value < min_value
       min_value = value
+      min_key=key
     end
   end
-  min_value
+  min_key
 end
