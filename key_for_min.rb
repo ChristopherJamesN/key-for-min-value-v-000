07806a46ea5958efa0906ac61147917308affd5b
Chris Nady <nady.christopher@gmail.com>
2017-09-07 19:22:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
-    elsif min_value = nil
+    elsif min_value == nil
       min_value = value
     end
   end
